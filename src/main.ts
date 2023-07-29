fff2a662a3b72617c2263fc18ddded94a63983b0
Juris-Berkulis <krztfjrbhm1@gmail.com>
2023-07-29 06:13:55 +0300

    1. Install the state manager "pinia" using the terminal command "npm install pinia".

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import './assets/styles/main.scss';
 
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+app.mount('#app');
